3bcfec080ad480cb182aae1212ded6e1adb42c15
Saucetray <justinsostreofficial@gmail.com>
2018-09-24 01:46:27 -0400

    Staging for IPv6!

--- IP Packet Reader/IPv6.c
+++ IP Packet Reader/IPv6.c
@@ -7,3 +7,14 @@
 //
 
 #include "IPv6.h"
+
+#define IPv6_Length 36
+
+
+void create_IPv6_packet(IPv6_Packet *packet, FILE *file, int8_t byte) {
+    packet->version = 6;
+    unsigned char buffer[IPv6_Length];
+    fread(buffer, sizeof(unsigned char), IPv6_Length - 1, file);
+    
+    packet->traffic_class = ((byte << 4) & 0x0F) | buffer[0];
+}
